e93c5726c1bf9b6711626eef92cc6c00f194d70c
test <example@test.com>
2022-09-16 05:35:00 +0900

    views修正

--- private_diary/diary/forms.py
+++ private_diary/diary/forms.py
@@ -1,5 +1,9 @@
 from email import message
 from django import forms
+from django.core.mail import EmailMessage
+import logging
+
+logger = logging.getLogger(__name__)
 
 class InquiryForm(forms.Form):
     name = forms.CharField(label="お名前",max_length=30)
@@ -9,3 +13,21 @@ class InquiryForm(forms.Form):
 
     def __init__(self,*args,**kwargs):
         super().__init__(*args,**kwargs)
+
+    def send_email(self):
+        name=self.cleaned_data['name']
+        email=self.cleaned_data['email']
+        title=self.cleaned_data['title']
+        message=self.cleaned_data['message']
+
+        from_email = "admin@example.com"
+
+        # to_list = ['satou71150@icloud.com']
+        to_list = []
+        to_list.append(email)
+        cc_list = ['']
+
+        message = EmailMessage(subject=title,body=message,from_email=from_email,to=to_list,cc=cc_list)
+        message.send()
+
+        logger.info("send_email_OK")
--- private_diary/diary/views.py
+++ private_diary/diary/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
+from .forms import InquiryForm
+import logging
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 
@@ -9,5 +14,11 @@ class IndexView(generic.TemplateView):
     template_name = "index.html"
 
 class InquiryView(generic.FormView):
-    template_name = "inquiry.html"
+    template_name = "form.html"
     form_class = InquiryForm
+    success_url = reverse_lazy('diary:index')
+
+    def form_valid(self, form):
+        form.send_email()
+        logger.info("form_valid_check")
+        return super().form_valid(form)
